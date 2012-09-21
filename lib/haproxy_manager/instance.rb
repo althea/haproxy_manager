2682f8dc5dde840c0dbc3d7492ca1a191caeb217
V Sreekanth <sreeix@gmail.com>
2012-09-22 02:38:42 +0530

    Fixing a few test bugs

--- lib/haproxy_manager/instance.rb
+++ lib/haproxy_manager/instance.rb
@@ -10,10 +10,10 @@ module HAProxyManager
 
     # Diables a server in the server in a backend for maintenance.
     # If backend is not specified then all the backends in which the serverid exists are disabled.
-
+    # A disabled server shows up as in Maintance mode.
     def disable(serverid, backend = nil)
       all_servers(serverid, backend).each do |item|
-        @socket.exec "disable server #{item[0]}/#{item[1]}", &@print_response
+        @socket.execute "disable server #{item[0]}/#{item[1]}", &@print_response
       end
     end
 
@@ -21,7 +21,7 @@ module HAProxyManager
     # If backend is not specified then all the backends in which the serverid exists are enabled.
     def enable(serverid, backend = nil)
       all_servers(serverid, backend).each do |item|
-        @socket.exec "enable server #{item[0]}/#{item[1]}", &@print_response
+        @socket.execute "enable server #{item[0]}/#{item[1]}", &@print_response
       end
     end
 
@@ -29,8 +29,20 @@ module HAProxyManager
       @backends.keys
     end
 
+    def info
+      @socket.execute( "show info").inject({}){|hash, item| x = item.split(":"); hash.merge(x[0].strip =>  x[1].strip)}
+    end
+
     def servers(backend = nil)
       backend.nil? ? @backends.values.flatten : @backends[backend]
+    end
+    
+    # resets Haproxy counters. If no option is specified backend and frontend counters are cleared, but
+    # cumulative counters are not cleared. The cumulative counters can be cleared by passing the option of
+    # all to the method, in that case all the counters are cleared. This is similar to a restart.
+    # This is useful to reset stats after for example an incident.
+    def reset_counters(option = "")
+      @socket.execute "clear counters {option}", &@print_response
     end
 
     private
--- spec/instance_spec.rb
+++ spec/instance_spec.rb
@@ -9,9 +9,10 @@ module HAProxyManager
        "foo-https-farm,preprod-app,0,0,0,3,60,6219,2577996,71804141,,0,,1,30,3,0,UP,12,1,0,559,137,45394,255774,,1,2,1,,1948,,2,0,,2,L7OK,200,109,0,5912,181,11,29,0,0,,,,501,0,",
        "foo-https-farm,preprod-bg,0,0,0,0,30,0,0,0,,0,,0,0,0,0,DOWN,5,1,0,4,4,2453494,4518368,,1,2,2,,0,,2,0,,0,L4CON,,0,0,0,0,0,0,0,0,,,,0,0,", 
        "foo-https-farm,preprod-test,0,0,0,0,30,0,0,0,,0,,0,0,0,0,DOWN,5,1,0,0,1,5017532,5017532,,1,2,3,,0,,2,0,,0,L4CON,,0,0,0,0,0,0,0,0,,,,0,0,"]
+      @info_response = ["Name: HAProxy", "Version: 1.5-dev11", "Release_date: 2012/06/04", "Nbproc: 1", "Process_num: 1", "Pid: 4084", "Uptime: 58d 3h50m53s", "Uptime_sec: 5025053", "Memmax_MB: 0", "Ulimit-n: 40029", "Maxsock: 40029", "Maxconn: 20000", "Hard_maxconn: 20000", "Maxpipes: 0", "CurrConns: 0", "PipesUsed: 0", "PipesFree: 0", "ConnRate: 0", "ConnRateLimit: 0", "MaxConnRate: 69", "Tasks: 10", "Run_queue: 1", "Idle_pct: 100", "node: some machine on ec3", "description: Our awesome load balancer"]
     end
     before(:each) do
-      HAPSocket.any_instance.expects(:execute).returns(@stat_response)
+      HAPSocket.any_instance.expects(:execute).once.returns(@stat_response)
       @instance = Instance.new("foo")
     end
 
@@ -32,27 +33,38 @@ module HAProxyManager
       end
     end
     
-    describe "enables servers" do
+    describe "enables/disables servers" do
       it "enables a server" do
-        HAPSocket.any_instance.expects(:exec).with('enable server foo-farm/preprod-bg')
+        HAPSocket.any_instance.expects(:execute).with('enable server foo-farm/preprod-bg')
         @instance.enable("preprod-bg", "foo-farm")
       end
 
       it "enables a all servers in multiple backends" do
-        HAPSocket.any_instance.expects(:exec).with('enable server foo-farm/preprod-bg')
-        HAPSocket.any_instance.expects(:exec).with('enable server foo-https-farm/preprod-bg')
+        HAPSocket.any_instance.expects(:execute).with('enable server foo-farm/preprod-bg')
+        HAPSocket.any_instance.expects(:execute).with('enable server foo-https-farm/preprod-bg')
         @instance.enable("preprod-bg")
       end
 
       it "disables a server" do
-        HAPSocket.any_instance.expects(:exec).with('disable server foo-farm/preprod-bg')
+        HAPSocket.any_instance.expects(:execute).with('disable server foo-farm/preprod-bg')
         @instance.disable("preprod-bg", "foo-farm")
       end
 
       it "disables a server in all backends" do
-        HAPSocket.any_instance.expects(:exec).with('disable server foo-farm/preprod-bg')
-        HAPSocket.any_instance.expects(:exec).with('disable server foo-https-farm/preprod-bg')
+        HAPSocket.any_instance.expects(:execute).with('disable server foo-farm/preprod-bg')
+        HAPSocket.any_instance.expects(:execute).with('disable server foo-https-farm/preprod-bg')
         @instance.disable("preprod-bg")
+      end
+    end
+
+    describe "info about haproxy" do
+      it "has description/version and uptime" do
+        HAPSocket.any_instance.expects(:execute).with("show info").returns(@info_response)
+        
+        info = @instance.info
+        info["description"].should == 'Our awesome load balancer'
+        info["Version"].should == '1.5-dev11'
+        info["Uptime"].should == '58d 3h50m53s'
       end
     end
   end
